25ba4dd255a7e44c43eb74a1f5e7d580840dcedf
manuelfranceschi <manuelfranceschineves@gmail.com>
2022-01-21 16:04:52 +0100

    inkme_beta

---
+++ app/src/main/java/im/betas/inkme_betav2/MainActivity.kt
@@ -0,0 +1,11 @@
+package im.betas.inkme_betav2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.tutorial_pt1)
+    }
+}
