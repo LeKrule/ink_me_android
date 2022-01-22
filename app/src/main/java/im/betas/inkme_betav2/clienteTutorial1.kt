f520b69315096fcaae6f2f5c5fd08b43678c03e0
manuelfranceschi <manuelfranceschineves@gmail.com>
2022-01-22 14:56:55 +0100

    inkme_beta

--- app/src/main/java/im/betas/inkme_betav2/clienteTutorial1.kt
+++ app/src/main/java/im/betas/inkme_betav2/clienteTutorial1.kt
@@ -3,11 +3,9 @@ package im.betas.inkme_betav2
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class clienteTutorial1 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.tutorial_tatu_pt1)
     }
-    //Metodo para el boton siguiente
-
 }
