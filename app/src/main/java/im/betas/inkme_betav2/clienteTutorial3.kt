9fd1152869c7af30ecd4099b8911c6bfd0d0f5ca
Aitor <aitor_enrique_gomis_tsapp1ma2021@cev.com>
2022-02-05 19:54:46 +0100

    Creacion de los viewcontrolles tutorial Cliente 1,2,3

--- app/src/main/java/im/betas/inkme_betav2/clienteTutorial3.kt
+++ app/src/main/java/im/betas/inkme_betav2/clienteTutorial3.kt
@@ -3,9 +3,9 @@ package im.betas.inkme_betav2
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class clienteTutorial1 : AppCompatActivity() {
+class clienteTutorial3 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.fragment_tutorial_cliente1)
+        setContentView(R.layout.fragment_tutorial_cliente3)
     }
 }
