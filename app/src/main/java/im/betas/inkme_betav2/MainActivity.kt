5ba7448eabf9e80f04575cc7d1f6e1afaf585233
aitor <aitorgomis@gmail.com>
2022-02-21 14:06:52 +0100

    Creacion de el tabbar dentro de la aplicacion (con pantallas de pegote)

--- app/src/main/java/im/betas/inkme_betav2/MainActivity.kt
+++ app/src/main/java/im/betas/inkme_betav2/MainActivity.kt
@@ -2,11 +2,19 @@ package im.betas.inkme_betav2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.tutorial_tatu_pt1)
+        setContentView(R.layout.activity_main)
+
+         val bottomNavView = findViewById<BottomNavigationView>(R.id.bottomnav)
+         val navController = findNavController(R.id.fragment)
+
+        bottomNavView.setupWithNavController(navController)
     }
     //Metodo para el boton siguiente
 
--- app/src/main/java/im/betas/inkme_betav2/clienteTutorial1.kt
+++ app/src/main/java/im/betas/inkme_betav2/clienteTutorial1.kt
@@ -3,9 +3,16 @@ package im.betas.inkme_betav2
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+
 class clienteTutorial1 : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.fragment_tutorial_cliente1)
+
+    }
+
+    fun siguiente(){
+
     }
 }
